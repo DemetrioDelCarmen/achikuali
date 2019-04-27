7df9b26d7be5e1c360fd27be8f361a9ee58b4e36
DemetrioDelCarmen <demeritho95@gmail.com>
2019-04-26 21:19:32 -0500

    Agregando detalles

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-(function(s){
-    $(document).ready(function () {
-    $(".navbar").hide();
-    $(".navbarScroll").hide();
+  $(document).ready(function () {
+        $('.kk').hide();
+                    $(window).scroll(function () {
+
+                        if ($(window).scrollTop() > 100) {
+                            $('nav').addClass('header');
+                        } else {
+                            $('nav').removeClass('header');
+                        }
+
+                    });
 
 
+  });
 
+  $(document).ready(function () {
 
-    $(window).scroll(function () {
+    $('.cardWell').hover(
+        // trigger when mouse hover
+        function () {
+            $(this).animate({
+                marginTop: "-=1%",
+            }, 300);
+        },
 
-        if ($(window).scrollTop() < 100) {
-            
-            $('.navbar').fadeIn();
-            $('.navbarScroll').fadeOut();
-            
+        // trigger when mouse out
+        function () {
+            $(this).animate({
+                marginTop: "0%"
+            }, 300);
         }
+    );
 
-    });
-
-    $(window).scroll(function () {
-    
-        if ($(window).scrollTop() > 100) {
-            
-            $('.navbarScroll').fadeIn();
-            
-        }
-
-    });
-    
-       
-       
 
 });
-
-}(jQuery));
